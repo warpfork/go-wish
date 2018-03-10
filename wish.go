34a74895618459304b09c14ef2987cd73173f167
Eric Myhre <hash@exultant.us>
2018-03-10 16:17:36 +0100

    Stub more of ShouldBe.
    Declining to finish right now because it's something of a rabbithole of
    reflection minutae, and this is simply not a feature I'm particularly
    interested in refining all the details of on Day 1.
    
    Documentation, though!
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- wish.go
+++ wish.go
@@ -2,6 +2,7 @@ package wish
 
 import (
 	"fmt"
+	"reflect"
 
 	"github.com/warpfork/go-wish/cmp"
 )
@@ -50,19 +51,40 @@ type options interface {
 
 // ShouldBe asserts that two values are *exactly* the same.
 //
-// For pointers, ShouldBe checks pointer equality (*not* a recursion into the values;
-// use ShouldEqual to recursively inspect the values).
-// For primitives, ShouldBe is the same as ShouldEqual.
-// For structs, ShouldBe rejects all values; use ShouldEqual for recursive compares.
+// In almost all cases, prefer ShouldEqual.
+// ShouldBe differs from ShouldEqual in that it does *not* recurse, and
+// thus can be used to explicitly check pointer equality.
 //
-// Typically, ShouldEqual is more appropriate to use.  ShouldBe is only useful
-// if you explicitly wish to check pointer equality.
+// For pointers, ShouldBe checks pointer equality.
+// Using ShouldBe on any kind of values which require recursion to meaningfully
+// compare (e.g., structs, maps, arrays) will be rejected, as will using
+// ShouldBe on any kind of value which is already never recursive (e.g. any
+// primitives) since you can already use ShouldEqual to compare these.
 func ShouldBe(actual interface{}, desire interface{}) (problem string, passed bool) {
-	//	actual_rv := reflect.ValueOf(actual)
-	//	desire_rv := reflect.ValueOf(desire)
-	return "", true
+	switch reflect.TypeOf(desire).Kind() {
+	case // primitives
+		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
+		reflect.Bool, reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128, reflect.UnsafePointer:
+		panic("use ShouldEqual instead of ShouldBe when comparing primitives")
+	case // recursives
+		reflect.Interface, reflect.Array, reflect.Map, reflect.Slice, reflect.Struct:
+		panic("use ShouldEqual instead of ShouldBe when comparing recursive values")
+	case reflect.Ptr:
+		panic("TODO")
+	case reflect.Func:
+		panic("TODO")
+	case reflect.Chan:
+		panic("TODO")
+	default:
+		panic("unknown kind")
+	}
 }
 
+// ShouldEqual asserts that two values are the same, examining the values
+// recursively as necessary.  Maps, slices, and structs are all
+// valid to compare with ShouldEqual.  Pointers will be traversed, and
+// comparison continues with the values referenced by the pointer.
 func ShouldEqual(actual interface{}, desire interface{}) (string, bool) {
 	s1, ok1 := actual.(string)
 	s2, ok2 := desire.(string)
